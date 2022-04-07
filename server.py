da18c503299296c659a5d0df14eca1072fb358e1
ayoubinat <1ayoub.semlali@gmail.com>
2022-04-07 20:36:18 +0000

    moving repetition

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "this is my first app "
-
-
 @app.route('/home')
-def home():
-    return "this is the page home "
+def index():
+    return render_template("home.html")
 
-@app.route('/ayoub')
+
+@app.route('/about')
+def home():
+    return render_template("about.html")
+
+@app.route('/contact')
 def k():
-    return "ayoubinat zwinanat "
+    return render_template("contact.html")
 
 if __name__=='__main__':
     app.run(debug=True)
