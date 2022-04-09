73dd88cf11481fbe4401f154d96564f9079c6d3d
ayoubinat <1ayoub.semlali@gmail.com>
2022-04-09 02:10:15 +0000

    adding database store.db and create two products hp and samsung with db.session.add and ......commit()

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 
 from socket import MsgFlag
+from sqlite3 import dbapi2
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask,render_template,request
 from datetime import date
+import sqlalchemy
+
 
 x=date.today()
 
@@ -53,6 +57,27 @@ laptop=[
 
 
 app = Flask(__name__)
+
+messages=[]
+
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///store.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db=SQLAlchemy(app)
+
+class products(db.Model):
+    id=db.Column(db.Integer ,primary_key=True)
+    titre=db.Column(db.String(20),nullable=False)
+    description=db.Column(db.Text,nullable=False)
+    prix=db.Column(db.Integer,nullable=False)
+    image=db.Column(db.String(20),nullable=False)
+
+class Message(db.Model):
+    id=db.Column(db.Integer ,primary_key=True)
+    nom=db.Column(db.String(20),nullable=False)
+    message=db.Column(db.Text,nullable=False)
+    mail=db.Column(db.String(20),nullable=False)
+    
+
 @app.route('/')
 def d():
     return render_template("home.html",now=x,title="home")
@@ -66,7 +91,7 @@ def index():
 @app.route('/about')
 def home():
     return render_template("about.html",now=x,title="about")
-messages=[]
+
 @app.route('/contact')
 def k():
     name=request.form.get("name")
