c092077d144b73385e25d3e0f0fa2b74da0a9c47
ayoubinat <1ayoub.semlali@gmail.com>
2022-04-16 04:04:56 +0000

    finish adding products to basket

--- server.py
+++ server.py
@@ -2,12 +2,11 @@ from socket import MsgFlag
 from sqlite3 import dbapi2
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask,render_template,request
-from datetime import date
-import sqlalchemy
-
+from datetime import date   
 # from form import Registrationform,Loginfrom
 
 x=date.today()
+carte=[]
 
 app = Flask(__name__)
 
@@ -33,33 +32,40 @@ class Message(db.Model):
 laptop=products.query.all()
 
 @app.route('/')
-def d():
+def default():
     return render_template("home.html",now=x,title="home")
 
 
-# @app.route('/home')
-# def index():
-#     return render_template("home.html",now=x,title="home",prods=laptop)
+@app.route('/home')
+def index():
+    return render_template("home.html",now=x,title="home",prods=laptop)
 
-
+@app.route('/add/<id>')
+def add(id):
+    row=products.query.get(id)
+    carte.append(row)
+    return render_template("panier.html",carte=carte,prod=laptop)
+    
+    
 @app.route('/about')
-def home():
+def about():
     return render_template("about.html",now=x,title="about")
 
 @app.route('/contact')
-def k():
+def contact():
     name=request.form.get("name")
     email=request.form.get("email")
     message=request.form.get("message")
-    msg={'name':name,'email':email,'message':message}
     return render_template("contact.html",now=x,title="contact")
 
 @app.route('/signup')
 def login():
     return render_template('')
-# @app.route('/panier')
-# def panier():
-#     return render_template("panier.html")
+
+
+@app.route('/panier')
+def panier():
+    return render_template("panier.html",prod=laptop)
 if __name__=='__main__':
     app.run(debug=True)
     
